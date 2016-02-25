85604637ad36d7047f773224745c6832e99fa91f
jpfriedman18 <jpfriedman18@gmail.com>
2016-02-25 11:56:38 -0500

    Add stock_purchase routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
+  resources :stock_purchases
 end
