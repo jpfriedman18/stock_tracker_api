d0184c56181516de117e93fb16b0794966983566
jpfriedman18 <jpfriedman18@gmail.com>
2016-02-25 11:43:32 -0500

    Associate users and stock_purchases

--- app/models/stock_purchase.rb
+++ app/models/stock_purchase.rb
@@ -1,3 +1,3 @@
 class StockPurchase < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user, inverse_of: :stock_purchases, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 #
 class User < ActiveRecord::Base
   include Authentication
+  has_many :stock_purchases, inverse_of: :user, foreign_key: 'user_id'
 end
