1f9501ea91f73903a2a769631d67c6356953d9c9
jpfriedman18 <jpfriedman18@gmail.com>
2016-02-29 12:27:21 -0500

    Get ticker symbol validation working

--- app/models/stock_purchase.rb
+++ app/models/stock_purchase.rb
@@ -1,3 +1,12 @@
 class StockPurchase < ActiveRecord::Base
   belongs_to :user, inverse_of: :stock_purchases, class_name: 'User'
+
+  validate :is_valid_ticker_symbol, on: :create
+
+  def is_valid_ticker_symbol
+    response = HTTParty.get('http://dev.markitondemand.com/Api/v2/Quote/json', {query: { symbol: ticker }})
+    unless JSON.parse(response.body)['Symbol'] == ticker
+      errors.add(:ticker, "not a valid ticker symbol")
+    end
+  end
 end
